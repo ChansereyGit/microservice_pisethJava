4de5d5af0e72e90e18f3480263a773739495cefa
ChansereyGit <samnangchanserey@gmail.com>
2025-07-17 17:02:57 +0700

    spring cloud

---
+++ account/src/main/java/org/example/account/config/AccountServiceConfig.java
@@ -0,0 +1,18 @@
+package org.example.account.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "account")
+public class AccountServiceConfig {
+    private String msg;
+    private String buildVersion;
+    private Map<String,String> mailDetails;
+    private List<String> activeBranches;
+}
---
+++ account/src/main/java/org/example/account/property/Properties.java
@@ -0,0 +1,18 @@
+package org.example.account.property;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Properties {
+    private String msg;
+    private String buildVersion;
+    private Map<String,String> mailDetails;
+    private List<String> activeBranches;
+}
